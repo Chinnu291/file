dd75e59f897a90a0238ddc3d95a1faaa38a69846
Chinnu291 <manikantagunturi29@gmail.com>
2025-05-04 17:29:08 +0530

    Add files via upload

---
+++ emoji_map.py
@@ -0,0 +1,15 @@
+emoji_map = {
+    'joy': '😊',
+    'sadness': '😢',
+    'anger': '😠',
+    'fear': '😨',
+    'love': '❤',
+    'surprise': '😲',
+    'disgust': '🤢',
+    'neutral': '😐',
+    'gratitude': '🙏',
+    'admiration': '👏',
+    'curiosity': '🤔',
+    'relief': '😌',
+    'nervousness': '😬'
+}
